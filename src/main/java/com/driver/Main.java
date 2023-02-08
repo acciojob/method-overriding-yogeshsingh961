dc476f02ddc68a7a94788ffc9249537657da77d4
Yogesh Singh <yogeshsingh3596@gmail.com>
2023-02-09 01:37:02 +0530

    class A & B inside main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,19 @@
 package com.driver;
 
-class A{
-    String meth(){
-        return "Invoking method from class A";
-    }
-}
 
-class B extends A{
-   @Override
-    String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
 public class Main {
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
   public static void main(String[]args){
       A b1= new B();
       b1.meth();
